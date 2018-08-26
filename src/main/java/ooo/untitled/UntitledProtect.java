00a049e20cfb8531dc5015e84cd2d5cf833cd9bf
Sotr <i@omc.hk>
2018-08-27 02:35:57 +0800

    Stash work

---
+++ src/main/java/ooo/untitled/UntitledProtect.java
@@ -0,0 +1,156 @@
+package ooo.untitled;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.SneakyThrows;
+
+import static ooo.untitled.VersionLevel.modernApi;
+
+import java.lang.reflect.Method;
+
+public class UntitledProtect extends JavaPlugin implements Listener {
+    private static UntitledProtect singleton;
+    
+    public UntitledProtect() {
+        singleton = this;
+    }
+    
+    public static UntitledProtect instance() {
+        return singleton;
+    }
+    
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+    
+    private Block cachedSourceBlock; // TODO Debug
+    // The value can be from 0 to 5 (6 may appear instantly, see below),
+    // each number represent an updated block around the source block.
+    private int physicsIndex = 0;
+    
+    // Reflection constant and helper
+    private final static Method IS_TICKED_METHOD = isTickedMethod();
+    @SneakyThrows
+    private static Method isTickedMethod() {
+        Class<?> nmsChunkClass = Class.forName("net.minecraft.server.v1_" + (modernApi() ? "13" : "12") + "_R1.Chunk");
+        return modernApi() ? nmsChunkClass.getMethod("v") : nmsChunkClass.getMethod("j"); // OBFHELPER - isTicked
+    }
+    private final static Method GET_HANDLE_METHOD = getHandleMethod();
+    @SneakyThrows
+    private static Method getHandleMethod() {
+        Class<?> craftChunkClass = Class.forName("org.bukkit.craftbukkit.v1_" + (modernApi() ? "13" : "12") + "_R1.CraftChunk");
+        Method getHandleMethod = craftChunkClass.getDeclaredMethod("getHandle");
+        getHandleMethod.setAccessible(true);
+        return getHandleMethod;
+    }
+    
+    // Material generic constant
+    private final static Material DOUBLE_PLANT = modernApi() ? Material.LEGACY_GRASS : Material.GRASS;
+    private final static Material GRASS = modernApi() ? Material.LEGACY_GRASS : Material.GRASS;
+    private final static Material DIRT = modernApi() ? Material.LEGACY_DIRT : Material.DIRT;
+    private final static Material SOIL = modernApi() ? Material.SOIL : Material.SOIL;
+    
+    @SneakyThrows
+    // Impl Note: Ignore the cancelleation will have a big chance to destroy our index,
+    // a safe way to ignore cancalleation is pass a boolean value to the log method.
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
+    /* 
+     * Introduction to implementation of physics listening:
+     * 2d view (above) of source block and its around blocks:
+     *                       □ 
+     *                     □ ■ □
+     *                       □ 
+     * 2d view (front) of source block and its around blocks:
+     *                       □
+     *                     □ ■ □
+     *                       □
+     * Actually, no difference but actually different view. 
+     * When the source block (center, black one) changed in some way,
+     * its around 6 blocks will be updated (i.e. apply physics) and
+     * trigger a `BlockPhysicsEvent` for each around block, however,
+     * the source block itself won't trigger a `BlockPhysicsEvent`.
+     * All updates from the same source block should have same changed
+     * type (the pervious type of source block).
+     */
+    public void onAnyApplyPhysics(BlockPhysicsEvent event) { // Confused with `any`, look at our custom events
+        Block source = event.getSourceBlock();
+        if (! (boolean) IS_TICKED_METHOD.invoke(GET_HANDLE_METHOD.invoke(source.getChunk()))) { // TODO Adapt each version to avoid reflection use
+            Bukkit.getLogger().warning("Skipped physics from unticked chunk @ from " + event.getChangedType().name() + " to " + source.getType().name());
+            Bukkit.getLogger().warning("At @ " + source.getX() + ", " + source.getY() + ", " + source.getZ());
+            return;
+        }
+        
+        // Impl Note: Only plants will causing an event whose source block is the same on the triggered block.
+        // By this way, the source block equals with the trigger block in reference.
+        if (event.getBlock() == source) {
+            Material previousType = event.getChangedType();
+            // Impl Note: Double plant is one of the special block, we special case it because we
+            // wanna capature the destroy of its upper plant which will not cause a physics event.
+            // when break it by destroy its lower plant by hand, see below for more information.
+            if (previousType != DOUBLE_PLANT)
+                performLogBlockDisappearance(source, previousType);
+            // Obviously no need to log those plant again.
+            return;
+        }
+        
+        Material changedType = event.getChangedType(); // Multiple used below
+        // TODO Debug start - single update
+        boolean nullCachedType = cachedSourceBlock == null;
+        if (!nullCachedType && cachedSourceBlock.getBlockKey() != source.getBlockKey()) {
+            // TODO this is shit, they are natural water speard or something like this, just return
+            //Bukkit.getLogger().severe("Real single update caught!");
+            //Bukkit.getLogger().severe("Position of source: " + source.getX() + ", " + source.getY() + ", " + source.getZ());
+            //Bukkit.getLogger().severe("Position of cached: " + cachedSourceBlock.getX() + ", " + cachedSourceBlock.getY() + ", " + cachedSourceBlock.getZ());
+            physicsIndex = 1; // We used one index after all
+            cachedSourceBlock = source; // Update type or we will dead-loop (high chance)
+            return;
+        }
+        // TODO Debug end
+        
+        // As you can see above, special blocks such as plant have been flitered,
+        // so any event reach here was triggered by a `regular` update which update 6 face
+        // blocks of the source block. However, we only wanna log once and capature the 6th
+        // block is the simplest way.
+        if (physicsIndex++ == 5) {
+            if (!event.isCancelled()) performLogBlockPhysics(source, changedType);
+            // Impl Note: Reset is important otherwise we will only log the first and only one block.
+            physicsIndex = 0;
+            source = null; // TODO Debug - no need to check when it's the first block of a physics round
+        } else {
+            if (nullCachedType) cachedSourceBlock = source; // TODO Debug - Null or not equals, no need to update every time
+        }
+    }
+    
+    // Coding style: the performance of a `static` method is slightly lower than a `non-static` method, however,
+    // a `static` word make things more clear, especially in multi-thread programming, so we choose it.
+    // In the above assumption, we shouldn't access a static field in our method or there will be a mess.
+    private static boolean lowerPlant(Block plant) {
+        // Impl Note: This method is more complex than its name, not only we use this to check whether the
+        // block is the lower part of a double plant, but also indicate whether there is a upper part.
+        // By this way, we can catch the missing physics event which you can find more information about above.
+        Material groundType = plant.getRelative(0, -1, 0).getType();
+        return groundType == GRASS || groundType == DIRT || groundType == SOIL;
+    }
+    
+    private static void performLogBlockPhysics(Block block, Material previousType) {
+        // Impl Note: At this moment, if we check the type of upper block, we
+        // will only get `AIR`, so specify the type of the upper plant is needed.
+        if (previousType == DOUBLE_PLANT && lowerPlant(block))
+            performLogBlockDisappearance(block.getRelative(0, 1, 0), DOUBLE_PLANT);
+        // Debug output for a regular (source) block change
+        //Bukkit.getLogger().info("Changed block: " + block.getType().name() + ", pervious type: " + previousType.name());
+    }
+    
+    private static void performLogBlockDisappearance(Block block /* get data from this in future */, Material previousType /* for double plants to manually specify type */) {
+        // Debug output for a disappeared block (single update - only plant and double plant for now)
+        //Bukkit.getLogger().warning("Disappeared block: " + previousType.name() + " (single)");
+    }
+}
---
+++ src/main/java/ooo/untitled/VersionLevel.java
@@ -0,0 +1,240 @@
+package ooo.untitled;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.bukkit.Bukkit;
+
+/**
+ * @author SotrForgotten
+ */
+public class VersionLevel {
+    @Documented
+    @Retention(RetentionPolicy.SOURCE)
+    // This is only a marker so far
+    public static @interface CallerSensitive {
+        String server() default "";
+        String version() default "";
+    }
+    
+    private final static Version level = checkServerAndApi();
+    private static boolean modernApi;
+    
+    private static boolean spigot;
+    private static boolean spigotInternalApi;
+    
+    private static boolean paper;
+    private static boolean paperViewDistanceApi;
+    
+    private static boolean forge;
+    
+    private static String rawVersion;
+    
+    public static final Version get() {
+        return level;
+    }
+    
+    public static final String rawVersion() {
+        return rawVersion;
+    }
+    
+    public static boolean modernApi() {
+        return modernApi;
+    }
+    
+    public static boolean isSpigot() {
+        return spigot;
+    }
+    
+    public static boolean canAccessSpigotInternalApi() {
+        return spigotInternalApi;
+    }
+    
+    public static boolean isPaper() {
+        return paper;
+    }
+    
+    public static boolean canAccessPaperViewDistanceApi() {
+        return paperViewDistanceApi;
+    }
+    
+    public static boolean isForge() {
+        return forge;
+    }
+    
+    private static Version checkServerAndApi() {
+        Version level = checkServerVersion();
+        checkServerType(rawVersion);
+        checkApiType(level);
+        return level;
+    }
+    
+    private static void checkServerType(String rawVersion) {
+        String bukkitVersion = rawVersion.toLowerCase();
+        boolean thermos = bukkitVersion.contains("thermos") || bukkitVersion.contains("contigo");
+        forge = thermos || bukkitVersion.contains("cauldron") || bukkitVersion.contains("mcpc") || bukkitVersion.contains("uranium");
+        paper = bukkitVersion.contains("paper") || bukkitVersion.contains("taco") || bukkitVersion.contains("torch") || bukkitVersion.contains("akarin");
+        if (paper || thermos || bukkitVersion.contains("spigot")) spigot = true;
+    }
+    
+    private static void checkApiType(Version level) {
+        modernApi = level.ordinal() <= Version.MINECRAFT_1_13_R1.ordinal();
+        try {
+            Class.forName("org.spigotmc.RestartCommand");
+            spigotInternalApi = true;
+        } catch (ClassNotFoundException ignored) {
+            spigotInternalApi = false;
+        }
+        try {
+            org.bukkit.entity.Player.class.getMethod("getViewDistance");
+            paperViewDistanceApi = true;
+        } catch (NoSuchMethodException | SecurityException ignored) {
+            paperViewDistanceApi = false;
+        }
+    }
+    
+    private static Version checkServerVersion() {
+        String version = rawVersion = Bukkit.getServer().getVersion();
+        
+        if (version.contains("1.13")) {
+            return Version.MINECRAFT_1_13_R1;
+        }
+        
+        if (version.contains("1.12")) {
+            return Version.MINECRAFT_1_12_R1;
+        }
+        
+        if (version.contains("1.11")) {
+            return Version.MINECRAFT_1_11_R1;
+        }
+        
+        if (version.contains("1.10")) {
+            return Version.MINECRAFT_1_10_R1;
+        }
+        
+        if (version.contains("1.9.4")) {
+            return Version.MINECRAFT_1_9_R2;
+        }
+        
+        if (version.contains("1.9")) {
+            return Version.MINECRAFT_1_9_R1;
+        }
+        
+        if (version.contains("1.8.8")) {
+            return Version.MINECRAFT_1_8_R3;
+        }
+        
+        if (version.contains("1.8.3")) {
+            return Version.MINECRAFT_1_8_R2;
+        }
+        
+        if (version.contains("1.8")) {
+            return Version.MINECRAFT_1_8_R1;
+        }
+        
+        if (version.contains("1.7.10")) {
+            return Version.MINECRAFT_1_7_R4;
+        }
+        
+        if (version.contains("1.7.9")) {
+            return Version.MINECRAFT_1_7_R3;
+        }
+        
+        if (version.contains("1.7.5")) {
+            return Version.MINECRAFT_1_7_R2;
+        }
+        
+        if (version.contains("1.7")) {
+            return Version.MINECRAFT_1_7_R1;
+        }
+        
+        if (version.contains("1.6.4")) {
+            return Version.MINECRAFT_1_6_R3;
+        }
+        
+        if (version.contains("1.6.2")) {
+            return Version.MINECRAFT_1_6_R2;
+        }
+        
+        if (version.contains("1.6.1")) {
+            return Version.MINECRAFT_1_6_R1;
+        }
+        
+        if (version.contains("1.5.2")) {
+            return Version.MINECRAFT_1_5_R3;
+        }
+        
+        if (version.contains("1.5.1")) {
+            return Version.MINECRAFT_1_5_R2;
+        }
+        
+        if (version.contains("1.5")) {
+            return Version.MINECRAFT_1_5_R1;
+        }
+        
+        if (version.contains("1.4.7")) {
+            return Version.MINECRAFT_1_4_R1;
+        }
+        
+        if (version.contains("1.4.6")) {
+            return Version.MINECRAFT_1_4_6;
+        }
+        
+        return Version.MINECRAFT_FUTURE;
+    }
+    
+    public static boolean isLowerThan(Version other) {
+        return level.ordinal() > other.ordinal();
+    }
+    
+    public static boolean isLowerEquals(Version other) {
+        return level.ordinal() >= other.ordinal();
+    }
+    
+    public static boolean isHigherThan(Version other) {
+        return level.ordinal() < other.ordinal();
+    }
+    
+    public static boolean isHigherEquals(Version other) {
+        return level.ordinal() <= other.ordinal();
+    }
+    
+    public static boolean equals(Version other) {
+        return level == other;
+    }
+    
+    // The order is important!
+    public enum Version {
+        MINECRAFT_FUTURE,
+        
+        MINECRAFT_1_13_R1,
+        
+        MINECRAFT_1_12_R1,
+        MINECRAFT_1_11_R1,
+        MINECRAFT_1_10_R1,
+        
+        MINECRAFT_1_9_R2,
+        MINECRAFT_1_9_R1,
+        
+        MINECRAFT_1_8_R3,
+        MINECRAFT_1_8_R2,
+        MINECRAFT_1_8_R1,
+        
+        MINECRAFT_1_7_R4,
+        MINECRAFT_1_7_R3,
+        MINECRAFT_1_7_R2,
+        MINECRAFT_1_7_R1,
+        
+        MINECRAFT_1_6_R3,
+        MINECRAFT_1_6_R2,
+        MINECRAFT_1_6_R1,
+        
+        MINECRAFT_1_5_R3,
+        MINECRAFT_1_5_R2,
+        MINECRAFT_1_5_R1,
+        
+        MINECRAFT_1_4_R1,
+        MINECRAFT_1_4_6
+    }
+}
