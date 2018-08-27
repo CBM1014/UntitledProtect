bb122c5f5dea0099eeb14ca3b41f15a183c39244
Sotr <i@omc.hk>
2018-08-27 13:36:36 +0800

    Fixes unneed log

--- src/main/java/ooo/untitled/UntitledProtect.java
+++ src/main/java/ooo/untitled/UntitledProtect.java
@@ -31,7 +31,8 @@ public class UntitledProtect extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
     
-    private Block cachedSourceBlock; // TODO Debug
+    // Cache the last source block so we can catch single updates
+    private Block cachedSourceBlock;
     // The value can be from 0 to 5 (6 may appear instantly, see below),
     // each number represent an updated block around the source block.
     private int physicsIndex = 0;
@@ -56,7 +57,7 @@ public class UntitledProtect extends JavaPlugin implements Listener {
     private final static Material DOUBLE_PLANT = modernApi() ? Material.LEGACY_GRASS : Material.GRASS;
     private final static Material GRASS = modernApi() ? Material.LEGACY_GRASS : Material.GRASS;
     private final static Material DIRT = modernApi() ? Material.LEGACY_DIRT : Material.DIRT;
-    private final static Material SOIL = modernApi() ? Material.SOIL : Material.SOIL;
+    private final static Material SOIL = modernApi() ? Material.LEGACY_SOIL : Material.SOIL;
     
     @SneakyThrows
     // Impl Note: Ignore the cancelleation will have a big chance to destroy our index,
@@ -65,13 +66,13 @@ public class UntitledProtect extends JavaPlugin implements Listener {
     /* 
      * Introduction to implementation of physics listening:
      * 2d view (above) of source block and its around blocks:
-     *                       □ 
+     *                       □  
      *                     □ ■ □
-     *                       □ 
+     *                       □  
      * 2d view (front) of source block and its around blocks:
-     *                       □
+     *                       □  
      *                     □ ■ □
-     *                       □
+     *                       □  
      * Actually, no difference but actually different view. 
      * When the source block (center, black one) changed in some way,
      * its around 6 blocks will be updated (i.e. apply physics) and
@@ -82,9 +83,7 @@ public class UntitledProtect extends JavaPlugin implements Listener {
      */
     public void onAnyApplyPhysics(BlockPhysicsEvent event) { // Confused with `any`, look at our custom events
         Block source = event.getSourceBlock();
-        if (! (boolean) IS_TICKED_METHOD.invoke(GET_HANDLE_METHOD.invoke(source.getChunk()))) { // TODO Adapt each version to avoid reflection use
-            Bukkit.getLogger().warning("Skipped physics from unticked chunk @ from " + event.getChangedType().name() + " to " + source.getType().name());
-            Bukkit.getLogger().warning("At @ " + source.getX() + ", " + source.getY() + ", " + source.getZ());
+        if (! (boolean) IS_TICKED_METHOD.invoke(GET_HANDLE_METHOD.invoke(source.getChunk()))) {
             return;
         }
         
@@ -97,42 +96,47 @@ public class UntitledProtect extends JavaPlugin implements Listener {
             // when break it by destroy its lower plant by hand, see below for more information.
             if (previousType != DOUBLE_PLANT)
                 performLogBlockDisappearance(source, previousType);
-            // Obviously no need to log those plant again.
             return;
         }
         
-        Material changedType = event.getChangedType(); // Multiple used below
-        // TODO Debug start - single update
-        boolean nullCachedType = cachedSourceBlock == null;
-        if (!nullCachedType && cachedSourceBlock.getBlockKey() != source.getBlockKey()) {
-            // TODO this is shit, they are natural water speard or something like this, just return
-            //Bukkit.getLogger().severe("Real single update caught!");
-            //Bukkit.getLogger().severe("Position of source: " + source.getX() + ", " + source.getY() + ", " + source.getZ());
-            //Bukkit.getLogger().severe("Position of cached: " + cachedSourceBlock.getX() + ", " + cachedSourceBlock.getY() + ", " + cachedSourceBlock.getZ());
-            physicsIndex = 1; // We used one index after all
-            cachedSourceBlock = source; // Update type or we will dead-loop (high chance)
-            return;
+        if (cachedSourceBlock == null) {
+            cachedSourceBlock = source; // No need to update every time
+        } else {
+            // A single update won't happen before a regular update finish on same source block
+            if (cachedSourceBlock.getBlockKey() != source.getBlockKey()) {
+                // Impl Note: Natural water spread from terrain generated water source
+                physicsIndex = 1; // We used one index after all - we dunno whether current physics is dummy!
+                cachedSourceBlock = source;
+                return;
+            }
         }
-        // TODO Debug end
         
         // As you can see above, special blocks such as plant have been flitered,
         // so any event reach here was triggered by a `regular` update which update 6 face
         // blocks of the source block. However, we only wanna log once and capature the 6th
         // block is the simplest way.
         if (physicsIndex++ == 5) {
-            if (!event.isCancelled()) performLogBlockPhysics(source, changedType);
+            if (!event.isCancelled()) {
+                Material previousType = event.getChangedType();
+                if (previousType == DOUBLE_PLANT && isLowerPlant(source)) {
+                    // Workaround missing physics event for upper part of plant
+                    // Impl Note: At this moment, if we check the type of upper block, we
+                    // will only get `AIR`, so specify the type of the upper plant is needed.
+                    performLogBlockDisappearance(source.getRelative(0, 1, 0), DOUBLE_PLANT);
+                } else {
+                    performLogBlockPhysics(source, previousType);
+                }
+            }
             // Impl Note: Reset is important otherwise we will only log the first and only one block.
             physicsIndex = 0;
-            source = null; // TODO Debug - no need to check when it's the first block of a physics round
-        } else {
-            if (nullCachedType) cachedSourceBlock = source; // TODO Debug - Null or not equals, no need to update every time
+            source = null; // No need to check when it's the first block of a physics round - we will ignore null, see above
         }
     }
     
     // Coding style: the performance of a `static` method is slightly lower than a `non-static` method, however,
     // a `static` word make things more clear, especially in multi-thread programming, so we choose it.
     // In the above assumption, we shouldn't access a static field in our method or there will be a mess.
-    private static boolean lowerPlant(Block plant) {
+    private static boolean isLowerPlant(Block plant) {
         // Impl Note: This method is more complex than its name, not only we use this to check whether the
         // block is the lower part of a double plant, but also indicate whether there is a upper part.
         // By this way, we can catch the missing physics event which you can find more information about above.
@@ -141,16 +145,10 @@ public class UntitledProtect extends JavaPlugin implements Listener {
     }
     
     private static void performLogBlockPhysics(Block block, Material previousType) {
-        // Impl Note: At this moment, if we check the type of upper block, we
-        // will only get `AIR`, so specify the type of the upper plant is needed.
-        if (previousType == DOUBLE_PLANT && lowerPlant(block))
-            performLogBlockDisappearance(block.getRelative(0, 1, 0), DOUBLE_PLANT);
-        // Debug output for a regular (source) block change
-        //Bukkit.getLogger().info("Changed block: " + block.getType().name() + ", pervious type: " + previousType.name());
+        
     }
     
     private static void performLogBlockDisappearance(Block block /* get data from this in future */, Material previousType /* for double plants to manually specify type */) {
-        // Debug output for a disappeared block (single update - only plant and double plant for now)
-        //Bukkit.getLogger().warning("Disappeared block: " + previousType.name() + " (single)");
+        
     }
 }
